39f6a3548fa6cfce51c54710ff3bfb432d7ced1e
Bishwash Adhikari <bishwashadhikari1@gmail.com>
2023-03-17 14:57:08 +0545

    android manifest added

--- lib/main.dart
+++ lib/main.dart
@@ -38,19 +38,38 @@ class _EVControlPageState extends State<EVControlPage> {
   bool _isPowerOn = true;
   int _speed = 0;
 
-  // this method to communicate with the Arduino Uno
-  Future<void> _sendMessage(String message) async {
+  void _toggleDirection() {
+    setState(() {
+      _isForward = !_isForward;
+    });
+    _sendToArduino();
+  }
+
+  void _toggleHeadlight() {
+    setState(() {
+      _isHeadlightOn = !_isHeadlightOn;
+    });
+    _sendToArduino();
+  }
+
+  void _togglePower() {
+    setState(() {
+      _isPowerOn = !_isPowerOn;
+    });
+  }
+
+  Future<void> _sendToArduino() async {
     List<UsbDevice> devices = await UsbSerial.listDevices();
     if (devices.isEmpty) {
       print('No USB devices found');
       return;
     }
-    UsbPort? port = await devices[0].create();
-    if (port == null) {
-      print('Failed to create a port');
+
+    UsbPort port;
+    if (devices.length == 0) {
       return;
     }
-
+    port = (await devices[0].create())!;
     bool openResult = await port.open();
     if (!openResult) {
       print("Failed to open");
@@ -59,32 +78,20 @@ class _EVControlPageState extends State<EVControlPage> {
 
     await port.setDTR(true);
     await port.setRTS(true);
-
     port.setPortParameters(
         9600, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
 
-    await port.write(Uint8List.fromList(message.codeUnits));
+    Uint8List dataToSend = Uint8List.fromList([
+      _isForward ? 1 : 0, // Send 1 for Forward, 0 for Reverse (D3)
+      _isHeadlightOn
+          ? 1
+          : 0 // Send 1 for Headlight ON, 0 for Headlight OFF (D4)
+    ]);
+
+    await port.write(dataToSend);
+
+    await Future.delayed(Duration(seconds: 1));
     await port.close();
-  }
-
-  void _toggleDirection() {
-    setState(() {
-      _isForward = !_isForward;
-    });
-    _sendMessage(_isForward ? 'F' : 'R'); // Add this line
-  }
-
-  void _toggleHeadlight() {
-    setState(() {
-      _isHeadlightOn = !_isHeadlightOn;
-    });
-    _sendMessage(_isHeadlightOn ? 'H_ON' : 'H_OFF'); // Add this line
-  }
-
-  void _togglePower() {
-    setState(() {
-      _isPowerOn = !_isPowerOn;
-    });
   }
 
   Future<void> _openGoogleMaps() async {
@@ -105,93 +112,99 @@ class _EVControlPageState extends State<EVControlPage> {
         height: double.infinity,
         child: Stack(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        '$_speed',
-                        style: TextStyle(
-                            fontSize: 48,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Dimensions'),
-                      ),
-                      Text(
-                        'kmph',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      AspectRatio(
-                        aspectRatio:
-                            1.0, // Adjust the aspect ratio as needed for your image
-                        child: LayoutBuilder(
-                          builder: (BuildContext context,
-                              BoxConstraints constraints) {
-                            return Container(
-                              width: constraints.maxWidth,
-                              height: constraints.maxHeight,
-                              child: Image.asset('assets/car_top_view.png',
-                                  fit: BoxFit.contain),
-                            );
-                          },
+            Positioned.fill(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '$_speed',
+                          style: TextStyle(
+                              fontSize: 48,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Dimensions'),
                         ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          FloatingActionButton(
-                            onPressed: _toggleDirection,
-                            child: Text(_isForward ? 'F' : 'R'),
-                          ),
-                          FloatingActionButton(
-                            onPressed: _toggleHeadlight,
-                            backgroundColor:
-                                _isHeadlightOn ? Colors.yellow : null,
-                            child: Icon(Icons.light_rounded),
-                          ),
-                          FloatingActionButton(
-                            onPressed: _openGoogleMaps,
-                            child: Icon(Icons.map),
-                          ),
-                        ],
-                      ),
-                    ],
+                        Text(
+                          'kmph',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Positioned(
-                  top: 10,
-                  right: 10,
-                  child: Column(
-                    children: [
-                      FloatingActionButton(
-                        onPressed: _togglePower,
-                        backgroundColor: _isPowerOn ? Colors.green : Colors.red,
-                        child: Icon(Icons.power_settings_new),
-                      ),
-                      SizedBox(height: 10),
-                      FloatingActionButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SettingsPage()),
-                          );
-                        },
-                        child: Icon(Icons.settings),
-                      ),
-                    ],
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        AspectRatio(
+                          aspectRatio:
+                              1.0, // Adjust the aspect ratio as needed for your image
+                          child: LayoutBuilder(
+                            builder: (BuildContext context,
+                                BoxConstraints constraints) {
+                              return Container(
+                                width: constraints.maxWidth,
+                                height: constraints.maxHeight,
+                                child: Image.asset('assets/car_top_view.png',
+                                    fit: BoxFit.contain),
+                              );
+                            },
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            FloatingActionButton(
+                              heroTag: 'directionButton',
+                              onPressed: _toggleDirection,
+                              child: Text(_isForward ? 'F' : 'R'),
+                            ),
+                            FloatingActionButton(
+                              heroTag: 'headlightButton',
+                              onPressed: _toggleHeadlight,
+                              backgroundColor:
+                                  _isHeadlightOn ? Colors.yellow : null,
+                              child: Icon(Icons.light_rounded),
+                            ),
+                            FloatingActionButton(
+                              heroTag: 'mapsButton',
+                              onPressed: _openGoogleMaps,
+                              child: Icon(Icons.map),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
+            ),
+            Positioned(
+              top: 10,
+              right: 10,
+              child: Column(
+                children: [
+                  FloatingActionButton(
+                    heroTag: 'powerButton',
+                    onPressed: _togglePower,
+                    backgroundColor: _isPowerOn ? Colors.green : Colors.red,
+                    child: Icon(Icons.power_settings_new),
+                  ),
+                  SizedBox(height: 10),
+                  FloatingActionButton(
+                    heroTag: 'settingsButton',
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SettingsPage()),
+                      );
+                    },
+                    child: Icon(Icons.settings),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
